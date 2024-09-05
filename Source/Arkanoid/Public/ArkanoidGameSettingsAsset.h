b21f10a55809781476aa9a7bded421016f3b5b75
VernamRD <vernamrdcross@mail.ru>
2024-09-05 14:36:57 +0300

    Add settings data asset

---
+++ Source/Arkanoid/Public/ArkanoidGameSettingsAsset.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ArkanoidGameSettingsAsset.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class ARKANOID_API UArkanoidGameSettingsAsset : public UDataAsset
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Cart");
+    float DefaultCartWidth;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Game");
+    int32 BallsCount;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Ball");
+    float DefaultBallVelocity;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Ball");
+    float DefaultBallSize;
+};
